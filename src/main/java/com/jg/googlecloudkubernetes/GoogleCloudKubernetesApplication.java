797c9b485c6ffc41214342cb7eb39873f4fce9be
Joseph Grech <joseph.grech@melitaltd.com>
2020-10-05 10:47:50 +0200

    Application only (Wrapper included).

---
+++ src/main/java/com/jg/googlecloudkubernetes/GoogleCloudKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.jg.googlecloudkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jg/googlecloudkubernetes/TestController.java
@@ -0,0 +1,15 @@
+package com.jg.googlecloudkubernetes;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
+}
